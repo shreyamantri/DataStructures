0a03a13be1d7e80eeefc849b1012c38a36290951
shreyamantri <42333832+shreyamantri@users.noreply.github.com>
2024-01-10 17:21:26 -0800

    Update HistoryTextHandler.java

--- projects/proj2/proj2a/main/HistoryTextHandler.java
+++ projects/proj2/proj2a/main/HistoryTextHandler.java
@@ -1,1 +1,31 @@
+package main;
 
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+import java.util.List;
+
+
+public class HistoryTextHandler extends NgordnetQueryHandler {
+
+    private NGramMap ngMap;
+    public HistoryTextHandler(NGramMap map) {
+        this.ngMap = map;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        String response = "";
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        for (String word: words) {
+            String temp = word + ": ";
+            response = response + temp;
+            response = response + ngMap.weightHistory(word, startYear, endYear).toString();
+            response += "\n";
+        }
+        return response;
+    }
+}

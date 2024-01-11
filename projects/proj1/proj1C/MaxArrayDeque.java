bc08ca33c888373817fc9bde7f16385d6a0a52e9
shreyamantri <42333832+shreyamantri@users.noreply.github.com>
2024-01-10 17:04:41 -0800

    Update MaxArrayDeque.java

--- projects/proj1/proj1C/MaxArrayDeque.java
+++ projects/proj1/proj1C/MaxArrayDeque.java
@@ -1,1 +1,32 @@
+package deque;
 
+//import java.lang.reflect.Array;
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+
+    private Comparator<T> comp;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        this.comp = c;
+    }
+
+    public T max() {
+        return max(comp);
+    }
+
+    public T max(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
+
+        T maximum = (T) this.get(0);
+        for (int i = 0; i < this.size(); i++) {
+            if (c.compare((T) this.get(i), maximum) > 0) {
+                maximum = (T) this.get(i);
+            }
+        }
+        return maximum;
+    }
+}

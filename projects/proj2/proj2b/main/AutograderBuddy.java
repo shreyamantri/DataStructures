84f61c47143ae4fc385602eac1119443360dd48d
shreyamantri <42333832+shreyamantri@users.noreply.github.com>
2024-01-10 17:40:40 -0800

    Update AutograderBuddy.java

--- projects/proj2/proj2b/main/AutograderBuddy.java
+++ projects/proj2/proj2b/main/AutograderBuddy.java
@@ -1,1 +1,17 @@
+package main;
 
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+
+
+public class AutograderBuddy {
+    /** Returns a HyponymHandler */
+    public static NgordnetQueryHandler getHyponymHandler(
+            String wordFile, String countFile,
+            String synsetFile, String hyponymFile) {
+
+        NGramMap ngm = new NGramMap(wordFile, countFile);
+        WordNet wdNet = new WordNet(hyponymFile, synsetFile);
+        return new HyponymsHandler(wdNet, ngm);
+    }
+}

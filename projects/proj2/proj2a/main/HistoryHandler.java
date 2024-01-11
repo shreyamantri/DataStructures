f727f0dcedea6458121ad629f92bfb3b072f038a
shreyamantri <42333832+shreyamantri@users.noreply.github.com>
2024-01-10 17:20:53 -0800

    Update HistoryHandler.java

--- projects/proj2/proj2a/main/HistoryHandler.java
+++ projects/proj2/proj2a/main/HistoryHandler.java
@@ -1,1 +1,40 @@
+package main;
 
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import ngrams.NGramMap;
+import ngrams.TimeSeries;
+import org.knowm.xchart.XYChart;
+import plotting.Plotter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryHandler extends NgordnetQueryHandler {
+
+    private NGramMap ngMap;
+
+    public HistoryHandler(NGramMap map) {
+        this.ngMap = map;
+    }
+
+    @Override
+    public String handle(NgordnetQuery q) {
+        List<String> words = q.words();
+        int startYear = q.startYear();
+        int endYear = q.endYear();
+
+        ArrayList<TimeSeries> lts = new ArrayList<>();
+        ArrayList<String> labels = new ArrayList<>();
+
+        for (String word: words) {
+            lts.add(ngMap.weightHistory(word, startYear, endYear));
+            labels.add(word);
+        }
+
+        XYChart chart = Plotter.generateTimeSeriesChart(labels, lts);
+        String encodedImage = Plotter.encodeChartAsString(chart);
+
+        return encodedImage;
+    }
+}
